f76b88b9deb4877b469d00f5538af9bccbc7f1be
wizdom <154702154+owizdom@users.noreply.github.com>
2025-10-20 00:31:42 +0200

    updating

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,69 +1,17 @@
 #!/usr/bin/python3
-"""
-Reddit API Top Ten Hot Posts
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
-    
-    Args:
-        subreddit (str): The subreddit to query
-        
-    Returns:
-        None: Prints the titles or None if invalid subreddit
-    """
-    # Reddit API endpoint for hot posts
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    
-    # Headers to identify the script (required by Reddit API)
-    headers = {
-        'User-Agent': 'RedditTopTen/1.0 by owzidom'
-    }
-    
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        # Make request without following redirects
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the response is a redirect (indicates invalid subreddit)
-        if response.status_code in [301, 302, 303, 307, 308]:
-            print("None")
-            return
-        
-        # Check if request was successful
-        if response.status_code != 200:
-            print("None")
-            return
-            
-        # Parse JSON response
-        data = response.json()
-        
-        # Check if we have posts data
-        if 'data' not in data or 'children' not in data['data']:
-            print("None")
-            return
-            
-        posts = data['data']['children']
-        
-        # Print titles of first 10 posts
-        for i, post in enumerate(posts[:10]):
-            if 'data' in post and 'title' in post['data']:
-                print(post['data']['title'])
-                
-    except requests.exceptions.RequestException:
-        print("None")
-    except (KeyError, ValueError):
-        print("None")
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
-        print("None")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+        print(None)
