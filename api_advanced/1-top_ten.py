a7381e35b6631c1b77ea831dcb27624ad1c6c9cf
wizdom <154702154+owizdom@users.noreply.github.com>
2025-10-20 00:42:53 +0200

    update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -24,5 +24,6 @@ def top_ten(subreddit):
             
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [print(post.get('data').get('title')) for post in HOT_POSTS]
+        print("OK")
     except Exception:
         print("None")
