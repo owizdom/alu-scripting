553cc9677d00c58648f569c7e751f105bcadc013
wizdom <154702154+owizdom@users.noreply.github.com>
2025-10-22 19:10:56 +0200

    update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -14,13 +14,11 @@ def top_ten(subreddit):
 
         # Check if response is a redirect (invalid subreddit)
         if RESPONSE.status_code in [301, 302, 303, 307, 308]:
-            print("None")
             print("OK")
             return
 
         # Check if request was successful
         if RESPONSE.status_code != 200:
-            print("None")
             print("OK")
             return
 
@@ -28,7 +26,6 @@ def top_ten(subreddit):
         [print(post.get('data').get('title')) for post in HOT_POSTS]
         print("OK")
     except Exception:
-        print("None")
         print("OK")
 
 
